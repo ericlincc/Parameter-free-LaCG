8c780ce4b151aee5a66f551d1f49269e94a2adce
Alejandro Carderera <alejandro.carderera@gmail.com>
2021-01-20 17:25:10 -0500

    Minor
    Add algorithms to __init__ file so that we can use the experiments_helper

--- pflacg/algorithms/__init__.py
+++ pflacg/algorithms/__init__.py
@@ -1,2 +1,4 @@
 from pflacg.algorithms._algorithms_utils import ExitCriterion
 from pflacg.algorithms.pflacg import ParameterFreeLaCG
+from pflacg.algorithms.fw_variants import FrankWolfe
+
--- pflacg/experiments/__init__.py
+++ pflacg/experiments/__init__.py
@@ -1,8 +1,14 @@
 from pflacg.experiments.feasible_regions import (
     ConvexHull,
 	ProbabilitySimplexPolytope,
+	L1UnitBallPolytope,
+	L2UnitBallPolytope,
+	spectrahedron,
+	flow_polytope,
 )
 from pflacg.experiments.objective_functions import (
     Quadratic,
     HuberLoss,
+	graphical_lasso,
+	logistic_regression,
 )
